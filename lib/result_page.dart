6557f45bb64b985c2c7634dae4c7c1f732787ab0
Maissen <maissen.developer500@gmail.com>
2025-09-29 10:46:45 +0100

    Create result_page.dart

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'form_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mon Formulaire',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FormPage(), // La page de formulaire est la page d'accueil
+    );
+  }
+}
