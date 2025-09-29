3689d58c0c3b81e24e2ab51890243de887ec6154
Maissen <maissen.developer500@gmail.com>
2025-09-29 10:49:04 +0100

    update

---
+++ lib/form_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'result_page.dart'; // Pour la navigation vers la page de résultats
+
+class FormPage extends StatefulWidget {
+  const FormPage({Key? key}) : super(key: key);
+
+  @override
+  _FormPageState createState() => _FormPageState();
+}
